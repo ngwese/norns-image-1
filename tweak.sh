df03f434044804eb086d91e429c4e36f9c8baafc
Greg Wuller <greg@afofo.com>
2019-09-01 17:00:26 -0700

    tmp: switch to local container image

--- tweak.sh
+++ tweak.sh
@@ -17,12 +17,4 @@ docker run --rm -ti \
   -v "${PWD}/output/graphs":/buildroot/output/graphs \
   -e BR2_EXTERNAL=/mycustombuildroot \
   -e BR2_GRAPH_OUT=png \
-  $IMAGE \
-  sh -c "make ${CONFIG} \
-    && make \
-    && BR2_GRAPH_DEPS_OPTS=\"--exclude host\" make graph-depends \
-    && mv output/graphs/graph-depends.dot output/graphs/graph-depends-target.dot \
-    && mv output/graphs/graph-depends.png output/graphs/graph-depends-target.png \
-    && make graph-depends \
-    && make graph-build \
-    && make graph-size"
+  $IMAGE
