f5b83114d15c1d72fd625e819201f0dd75d7a16c
Greg Wuller <greg@afofo.com>
2019-09-02 23:22:21 -0700

    norns: explicitly set supercollider install prefix

--- package/norns/norns.mk
+++ package/norns/norns.mk
@@ -10,7 +10,7 @@ NORNS_SITE = $(call github,ngwese,norns,$(NORNS_VERSION))
 NORNS_LICENSE = ISC
 NORNS_LICENSE_FILES = LICENSE.txt
 NORNS_INSTALL_STAGING = YES
-NORNS_CONF_OPTS = --boost-includes=$(STAGING_DIR)/usr/include --boost-libs $(STAGING_DIR)/usr/lib
+NORNS_CONF_OPTS = --supercollider-prefix=$(STAGING_DIR)/usr --boost-includes=$(STAGING_DIR)/usr/include --boost-libs $(STAGING_DIR)/usr/lib
 NORNS_DEPENDENCIES = avahi boost cairo libevdev liblo libmonome lua nanomsg supercollider
 
 $(eval $(waf-package))
