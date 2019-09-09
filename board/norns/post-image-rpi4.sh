188357852d08a4ba2ab5aa3d34ac4eeeb6c9a862
Greg Wuller <greg@afofo.com>
2019-09-08 23:32:06 -0700

    post-image-rpi4: allow overlays to be enabled individually

--- board/norns/post-image-rpi4.sh
+++ board/norns/post-image-rpi4.sh
@@ -45,6 +45,52 @@ __EOF__
 		gpu_mem="${arg:2}"
 		sed -e "/^${gpu_mem%=*}=/s,=.*,=${gpu_mem##*=}," -i "${BINARIES_DIR}/rpi-firmware/config.txt"
 		;;
+		--add-norns-software-shutdown-overlay)
+		echo "Enabling norns software shutdown"
+		cat << __EOF__ >> "${BINARIES_DIR}/rpi-firmware/config.txt"
+
+# Enable software shutdown
+dtoverlay=gpio-poweroff:gpiopin=12,active_low=1
+__EOF__
+		;;
+		--add-norns-display-overlay)
+		echo "Enabling norns display"
+		cat << __EOF__ >> "${BINARIES_DIR}/rpi-firmware/config.txt"
+
+# Display
+# Connected using SPI
+dtparam=spi=on
+dtoverlay=ssd1322-spi
+__EOF__
+		;;
+		--add-norns-soundcard-overlay)
+		echo "Enabling norns soundcard"
+		cat << __EOF__ >> "${BINARIES_DIR}/rpi-firmware/config.txt"
+
+# Soundcard
+# Connected using I2S
+# CS4270 codec used by soundcard connected using I2C
+dtparam=i2s=on
+dtparam=i2c=on
+dtoverlay=monome-snd
+__EOF__
+		;;
+		--add-norns-battery-overlay)
+		echo "Enabling norns battery management"
+		cat << __EOF__ >> "${BINARIES_DIR}/rpi-firmware/config.txt"
+
+# Battery management
+dtoverlay=bq27441
+__EOF__
+		;;
+		--add-norns-control-overlay)
+		echo "Enabling norns buttons and encoders"
+		cat << __EOF__ >> "${BINARIES_DIR}/rpi-firmware/config.txt"
+
+# Buttons and encoders
+dtoverlay=norns-buttons-encoders
+__EOF__
+		;;
 	esac
 
 done
