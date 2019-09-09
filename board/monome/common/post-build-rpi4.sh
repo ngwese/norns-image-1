bf346b362ca0a3162cbd7cb6266673c0dd988cca
Greg Wuller <greg@afofo.com>
2019-09-08 23:28:36 -0700

    post-build-rpi4: manually copy norns overlays

--- board/monome/common/post-build-rpi4.sh
+++ board/monome/common/post-build-rpi4.sh
@@ -12,6 +12,15 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
 
+# ugly hack to get custom overlays into the image
+OVERLAYS="monome-snd ssd1322-spi norns-buttons-encoders"
+OVERLAYS_BUILD_OUTPUT=${BUILD_DIR}/linux-custom/arch/arm/boot/dts/overlays
+OVERLAYS_TARGET=${BINARIES_DIR}/rpi-firmware/overlays
+for overlay in $OVERLAYS
+do
+	cp -v ${OVERLAYS_BUILD_OUTPUT}/${overlay}.dtbo ${OVERLAYS_TARGET}/
+done
+
 # break out into common post-build
 ln -fs ../norns.target \
 		${TARGET_DIR}/etc/systemd/system/multi-user.target.wants/norns.target
