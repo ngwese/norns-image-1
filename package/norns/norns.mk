d45ddb0be877e0bb27b1ad61aa183d446665f686
Greg Wuller <greg@afofo.com>
2019-11-02 22:43:50 -0700

    Update norns package to v2.2.2

--- package/norns/norns.mk
+++ package/norns/norns.mk
@@ -4,10 +4,9 @@
 #
 ################################################################################
 
-#NORNS_VERSION = v2.0.1
-NORNS_VERSION = 367c7904157d0ade348e0aa50a8d9321b8fda28a
-NORNS_SITE = $(call github,ngwese,norns,$(NORNS_VERSION))
-NORNS_LICENSE = ISC
+NORNS_VERSION = v2.2.2
+NORNS_SITE = $(call github,monome,norns,$(NORNS_VERSION))
+NORNS_LICENSE = GPL-3.0
 NORNS_LICENSE_FILES = LICENSE.txt
 NORNS_INSTALL_STAGING = YES
 NORNS_CONF_OPTS = --supercollider-prefix=$(STAGING_DIR)/usr --boost-includes=$(STAGING_DIR)/usr/include --boost-libs $(STAGING_DIR)/usr/lib
