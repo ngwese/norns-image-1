4c2be988f5ceb1bd30220ff7586718a9bc0d59c6
Greg Wuller <greg@afofo.com>
2019-11-02 22:39:11 -0700

    Update maiden package to v1.0

--- package/maiden/maiden.mk
+++ package/maiden/maiden.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-MAIDEN_VERSION = v0.13
+MAIDEN_VERSION = v1.0
 MAIDEN_SOURCE = maiden-$(MAIDEN_VERSION).tgz
 MAIDEN_SITE = https://github.com/monome/maiden/releases/download/$(MAIDEN_VERSION)
 MAIDEN_LICENSE = GPL-3.0
 MAIDEN_LICENSE_FILES = LICENSE.txt
-MAIDEN_DEPENDENCIES = dust
+MAIDEN_DEPENDENCIES = dust ca-certificates
 
 define MAIDEN_INSTALL_TARGET_CMDS
 	# executable
@@ -19,6 +19,10 @@ define MAIDEN_INSTALL_TARGET_CMDS
 	cp -av $(@D)/app/build $(TARGET_DIR)/usr/share/maiden/app
 	# config
 	cp -av $(MAIDEN_PKGDIR)/maiden.yaml $(TARGET_DIR)/etc
+	# sources
+	mkdir -pv $(TARGET_DIR)/root/dust/data/{sources,catalogs}
+	cp -av $(@D)/dist/sources/*.json $(TARGET_DIR)/root/dust/data/sources/
+
 endef
 
 $(eval $(generic-package))
